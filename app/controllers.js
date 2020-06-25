dec3d638a7f145502f0013044472e4be23929654
Ketan Ghumatkar <ketan@avaamo.com>
2020-06-25 15:44:05 +0530

    added create agent popup

--- app/controllers.js
+++ app/controllers.js
@@ -1,34 +1,79 @@
-function ListaComprasController($scope) {
+angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
-    $scope.item = {};
+angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
+    var $ctrl = this;
 
-    $scope.itens = [
-        {name: 'Leite', owner: 'Ketan Ghumatkar'},
-        {name: 'Cerveja', owner: 'Vignesh Shanbhang'}
+    $ctrl.bot = {};
+
+    $ctrl.bots = [
+        {
+            first_name: 'Leite',
+            owner: 'Ketan Ghumatkar',
+            description: 'Leite description'
+        },
+        {
+            first_name: 'Cerveja',
+            owner: 'Vignesh Shanbhang',
+            description: 'Cerveja description'
+        }
     ];
 
-    $scope.adicionaItem = function () {
-        $scope.itens.push({
-            name: $scope.item.name,
-            owner: $scope.item.owner
-        });
-        $scope.item.name = $scope.item.owner = '';
-        toastr.success("Agent added successfully");
+    $ctrl.editarItem = function(index){
+        $ctrl.item = $ctrl.bots[index];
+        $ctrl.edit = true;
     };
 
-    $scope.editarItem = function(index){
-        $scope.item = $scope.itens[index];
-        $scope.edit = true;
-    };
-
-    $scope.applyChanges = function(index){
-        $scope.item = {};
-        $scope.edit = false;
+    $ctrl.applyChanges = function(index){
+        $ctrl.item = {};
+        $ctrl.edit = false;
         toastr.success("Item alterado com sucesso.");
     };
 
-    $scope.deleteItem = function(index){
-        $scope.itens.splice(index, 1);
+    $ctrl.deleteItem = function(index){
+        $ctrl.bots.splice(index, 1);
         toastr.success("Item removido com sucesso.");
     };
-}
+
+    $ctrl.open = function (size, parentSelector) {
+        var parentElem = parentSelector ?
+            angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+        var modalInstance = $uibModal.open({
+            animation: $ctrl.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            controllerAs: '$ctrl',
+            size: size,
+            appendTo: parentElem,
+            resolve: {
+                bot: function () {
+                    return $ctrl.bot;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (bot) {
+            console.log(bot)
+            $ctrl.bots.push(bot)
+            $ctrl.bot = {}
+            toastr.success("Agent added successfully");
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+});
+
+angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($uibModalInstance, bot) {
+    var $ctrl = this;
+    $ctrl.bot = bot;
+
+    $ctrl.ok = function () {
+        $uibModalInstance.close($ctrl.bot);
+    };
+
+    $ctrl.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
